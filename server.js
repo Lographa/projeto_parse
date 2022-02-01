ca9c9ae5f28dd5be977db59fb4f59624a3ddc13d
Lographa <81043894+Lographa@users.noreply.github.com>
2022-01-31 22:53:58 -0300

    segundo commit
    Nesse commit contem os avanço do projeto no arquivo server.js

--- server.js
+++ server.js
@@ -1,19 +1,63 @@
+const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
-const puppeteer = require('puppeteer');
+
 
 //configuração da porta
 app.listen(3000, function () {
     console.log(`Rodando na porta 3000`);
 });
 
-//abri o browser
-puppeteer.launch().then(async function(browser) {
-    const page = await browser.newPage();
-    await page.goto('https://www.fflogs.com/reports/qP7hr1nFy3dx2ZQw#fight=3&type=damage-done');
+app.get('/busca', (req, res) => {
+    const ola = puppeteer.launch().then(async function(browser) {
+        const page = await browser.newPage();
+        await page.goto('https://www.fflogs.com/reports/qP7hr1nFy3dx2ZQw#fight=3&type=damage-done');
+    
+        // const parsenumero = '#main-table-0 tbody tr td:nth-child(1)  a';
+        // const parsenomers = '#main-table-0 tbody tr td:nth-child(2)  a';
 
-    await page.screenshot({path: 'fflog-website.png'});
+        const parseNome = await page.$$eval('#main-table-0 tbody tr td:nth-child(2)  a', function(parses) {
+            // Mapeia todos os nome 
+                return parses.map(function(parse) {
+              return parse.innerText;
+            });
+        });
 
-    //fecha
-    await browser.close();
+        const parseNumber = await page.$$eval('#main-table-0 tbody tr td:nth-child(1)  a', function(parses) {
+            // Mapei todos os parses
+                return parses.map(function(parse) {
+              return parse.innerText;
+            });
+        });
+
+
+        // const resposta = await Promise.all([parseNome, parseNumber]).then(data => {
+        //     const dataNome = data[0];
+        //     const dataParse = data[1];
+        //     const novaData = {};
+
+        //     dataParse.forEach(function (k, i) {
+        //         novaData[k] = dataNome[i]
+        //     })
+            
+        //     // console.log(novaData)
+        // });
+
+        //Para poder criar um JSON com 2 arrays, eu precisei criar uma condição
+        //como não consegui chegar da forma que eu queria, então criei esse return abaixo
+        const resposta = await Promise.all(parseNumber.map(async function (k, i) {
+            const dataNome = parseNome;
+            return '"' + k + '"' + ": " + '"' + dataNome[i] + '"';
+             
+        }));
+        
+        const stringando = '{';
+
+        //retorna uma resposta da chamada GET contendo a string do parse com nome do player
+        res.send(stringando.concat(resposta.toString(), '}'));
+        await browser.close();
+    });
+
+
 });
+
